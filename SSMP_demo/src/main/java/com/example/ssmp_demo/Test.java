e763589a7727cff6b28f3acff8cecbf3711391f6
panda-zzy <1269159491@qq.com>
2022-06-08 02:37:28 +0800

    test

--- SSMP_demo/src/main/java/com/example/ssmp_demo/Test.java
+++ SSMP_demo/src/main/java/com/example/ssmp_demo/Test.java
@@ -6,6 +6,8 @@ package com.example.ssmp_demo;
  */
 public class Test {
     public static void main(String[] args) {
+        System.out.println("test3");
+        System.out.println("test2");
         System.out.println("test");
     }
 
